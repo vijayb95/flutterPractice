dd1c8f3f9fef3bcc70c8f281003517e1e919128a
Vijayabalaji B <jayb2495@gmail.com>
2021-01-27 11:42:54 +0530

    starting

---
+++ assignment/lib/main.dart
@@ -0,0 +1,26 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some text
+// below it
+// 2) Add a button which changes the text (to any other text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Assignment1'),
+        ),
+        body: Text(
+          'this is a sample Text',
+          style: TextStyle(fontSize: 25),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
